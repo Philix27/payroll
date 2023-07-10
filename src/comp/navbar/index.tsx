5a7114c0a4961002dc1ff50bd44d86edfaa2a7eb
philix27 <philixbob@gmail.com>
2023-07-10 19:26:35 +0100

    chore: connect and signin with wallet

--- src/comp/navbar/index.tsx
+++ src/comp/navbar/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import { useAddress, ConnectWallet, useDisconnect } from "@thirdweb-dev/react";
+import { useRouter } from "next/router";
 
 export default function NavComp() {
+  const wallet_address = useAddress();
+  const disconnect = useDisconnect();
+  const router = useRouter();
   return (
     <nav className={styles.nav} id="section2">
       <div className={styles.brand}>
@@ -11,26 +16,48 @@ export default function NavComp() {
         </Link>
       </div>
       <div className={styles.nav_links}>
-        <div className={styles.nav_item}>
-          <Link href="#solutions">
-            <p>Solution</p>
-          </Link>
-        </div>
-        <div className={styles.nav_item}>
-          <Link href="#use_case">
-            <p>Use case</p>
-          </Link>
-        </div>
-        <div className={styles.nav_item}>
-          <Link href="#about">
-            <p>About</p>
-          </Link>
-        </div>
-        <div className={styles.nav_item}>
-          <Link href="/profile">
-            <p>Profile</p>
-          </Link>
-        </div>
+        {router.route === "/" && (
+          <div className={styles.nav_item}>
+            <Link href="#solutions">
+              <p>Solution</p>
+            </Link>
+          </div>
+        )}
+
+        {router.route === "/" && (
+          <div className={styles.nav_item}>
+            <Link href="#use_case">
+              <p>Use case</p>
+            </Link>
+          </div>
+        )}
+        {router.route === "/" && (
+          <div className={styles.nav_item}>
+            <Link href="#about">
+              <p>About</p>
+            </Link>
+          </div>
+        )}
+        {router.route === "/" && (
+          <div className={styles.nav_item}>
+            <Link href="/profile">
+              <p>Go to profile</p>
+            </Link>
+          </div>
+        )}
+
+        {!wallet_address && (
+          <div className={styles.nav_item}>
+            <ConnectWallet />
+          </div>
+        )}
+        {wallet_address && (
+          <div className={styles.nav_item} onClick={disconnect}>
+            <Link href="/">
+              <p>Disconnect</p>
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
--- src/views/profile/index.tsx
+++ src/views/profile/index.tsx
@@ -4,8 +4,11 @@ import TopSection from "./top_section";
 import NavComp from "@/src/comp/navbar";
 import HomeFooter from "./footer";
 import SecondSection from "./section";
+import { useContract } from "@thirdweb-dev/react";
+import { CONTRACT_ADDRESS } from "@/libs/address";
 
 export default function ProfileView() {
+  const { contract } = useContract(CONTRACT_ADDRESS);
   return (
     <div className={styles.container} id="container">
       <NavComp />
