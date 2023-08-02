a683654b7478f988167223d35dc27a1210fc566a
philix27 <philixbob@gmail.com>
2023-08-02 18:48:24 +0100

    updated header

--- libs/sanity.ts
+++
@@ -1,4 +0,0 @@
-import { SanityClient } from "@sanity/client";
-
-// export const client = new SanityClient({});
-export const client = "";
--- src/comp/global/Header.tsx
+++ src/comp/global/Header.tsx
@@ -15,8 +15,8 @@ export default function MetaHeader(props: IProps) {
         <meta charSet="UTF-8" />
         <meta name="description" content={props.description} />
         <meta name="keywords" content={props.keywords} />
-        <meta name="author" content="Unwind." />
-        <meta name="company" content="Unwind." />
+        <meta name="author" content="PaySync." />
+        <meta name="company" content="PaySync." />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/images/Unwind.png" />
       </Head>
